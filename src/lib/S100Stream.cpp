4c3409bdc9b57c5c970ba3c83d1572d519ef4c5b
dcentelles <centelld@uji.es>
2017-09-05 09:45:13 +0200

    beautify code using clang-format

--- src/lib/S100Stream.cpp
+++ src/lib/S100Stream.cpp
@@ -5,88 +5,74 @@
  *      Author: centelld
  */
 
-#include <dccomms_utils/S100Stream.h>
 #include <cstdint>
+#include <dccomms_utils/S100Stream.h>
 #include <string>
 
 namespace dccomms_utils {
 
-S100Stream::S100Stream(std::string serialportname, SerialPortStream::BaudRate baudrate, int maxBaudrate):
-        SerialPortStream(serialportname.c_str(), baudrate)
-{
-    _maxBaudrate = maxBaudrate;
-	init();
+S100Stream::S100Stream(std::string serialportname,
+                       SerialPortStream::BaudRate baudrate, int maxBaudrate)
+    : SerialPortStream(serialportname.c_str(), baudrate) {
+  _maxBaudrate = maxBaudrate;
+  init();
 }
 
-S100Stream::~S100Stream() {
+S100Stream::~S100Stream() {}
 
+void S100Stream::init() {
+  if (_maxBaudrate > 0)
+    _byteTransmissionTime = 1000. / (_maxBaudrate / 8.);
+
+  _maxTrunkSize = 100;
+  Log->debug("baudrate: {} ; byte transmission time: {} ; frame trunk size: {}",
+             _maxBaudrate, _byteTransmissionTime, _maxTrunkSize);
 }
 
-void S100Stream::init()
-{
-    if(_maxBaudrate > 0)
-        _byteTransmissionTime =  1000./( _maxBaudrate / 8.);
-
-    _maxTrunkSize = 100;
-    Log->debug("baudrate: {} ; byte transmission time: {} ; frame trunk size: {}",
-               _maxBaudrate,
-               _byteTransmissionTime,
-               _maxTrunkSize
-               );
+int S100Stream::_Recv(void *dbuf, int n, bool block) {
+  return Read((unsigned char *)dbuf, n, (unsigned int)block);
 }
 
-int S100Stream::_Recv(void * dbuf, int n, bool block)
-{
-	return Read((unsigned char *) dbuf, n, (unsigned int)block);
-}
+ICommsLink &S100Stream::operator<<(const DataLinkFramePtr &dlf) {
+  auto buffer = dlf->GetFrameBuffer();
+  auto fs = dlf->GetFrameSize();
 
-ICommsLink& S100Stream::operator<< (const DataLinkFramePtr & dlf)
-{
-    auto buffer = dlf->GetFrameBuffer ();
-    auto fs = dlf->GetFrameSize ();
+  auto ptr = buffer;
+  auto maxPtr = buffer + fs;
 
-    auto ptr = buffer;
-    auto maxPtr = buffer + fs;
+  unsigned int _frameTransmissionTime = ceil(fs * _byteTransmissionTime);
+  Log->debug("TX {}->{}: estimated frame transmission time: {} ms (FS: {}).",
+             dlf->GetSrcDir(), dlf->GetDesDir(), _frameTransmissionTime, fs);
 
-    unsigned int _frameTransmissionTime = ceil(fs * _byteTransmissionTime);
-    Log->debug("TX {}->{}: estimated frame transmission time: {} ms (FS: {}).",
-               dlf->GetSrcDir (),
-               dlf->GetDesDir (),
-               _frameTransmissionTime, fs);
+  unsigned int _trunkTransmissionTime;
 
-    unsigned int _trunkTransmissionTime;
+  _trunkTransmissionTime = ceil(_maxTrunkSize * _byteTransmissionTime);
 
-    _trunkTransmissionTime = ceil(_maxTrunkSize * _byteTransmissionTime);
+  while (ptr + _maxTrunkSize < maxPtr) {
+    Log->debug("Sending trunk of {} bytes... ({} ms)", _maxTrunkSize,
+               _trunkTransmissionTime);
+    Write(ptr, _maxTrunkSize);
+    std::this_thread::sleep_for(
+        std::chrono::milliseconds(_trunkTransmissionTime));
+    ptr += _maxTrunkSize;
+  }
 
-    while(ptr + _maxTrunkSize < maxPtr)
-    {
-        Log->debug("Sending trunk of {} bytes... ({} ms)",
-                   _maxTrunkSize,
-                   _trunkTransmissionTime);
-        Write(ptr, _maxTrunkSize);
-        std::this_thread::sleep_for(std::chrono::milliseconds(_trunkTransmissionTime));
-        ptr += _maxTrunkSize;
-    }
+  uint8_t endOfPacket[2] = {0xd, 0xa};
+  unsigned long left = maxPtr - ptr;
+  if (left > 0) {
+    _trunkTransmissionTime = ceil((left + 2) * _byteTransmissionTime);
+    Log->info("Sending trunk of {} bytes and end of packet... ({} ms)", left,
+              _trunkTransmissionTime);
+    Write(ptr, left);
+  } else {
+    _trunkTransmissionTime = ceil(2 * _byteTransmissionTime);
+    Log->info("Sending end of packet... ({} ms)", _trunkTransmissionTime);
+  }
+  Write(endOfPacket, 2);
+  std::this_thread::sleep_for(
+      std::chrono::milliseconds(_trunkTransmissionTime));
 
-    uint8_t endOfPacket[2] = {0xd,0xa};
-    unsigned long left = maxPtr - ptr;
-    if(left > 0)
-    {
-        _trunkTransmissionTime = ceil((left+2) * _byteTransmissionTime);
-        Log->info("Sending trunk of {} bytes and end of packet... ({} ms)",
-                   left,
-                   _trunkTransmissionTime);
-        Write(ptr, left);
-    }
-    else
-    {
-        _trunkTransmissionTime = ceil(2 * _byteTransmissionTime);
-        Log->info("Sending end of packet... ({} ms)", _trunkTransmissionTime);
-    }
-    Write(endOfPacket, 2);
-    std::this_thread::sleep_for(std::chrono::milliseconds(_trunkTransmissionTime));
-
-    return *this;
+  return *this;
 }
 
 } /* namespace merbots */
