8d0ff60094a516a637ee2aef03c6d78ade18260e
dcentelles <centelld@uji.es>
2018-05-28 11:59:34 +0200

    simplify bridges

--- src/main/dccomms_s100_bridge/dccomms_s100_bridge.cpp
+++ src/main/dccomms_s100_bridge/dccomms_s100_bridge.cpp
@@ -1,8 +1,8 @@
 #include <cpputils/SignalManager.h>
 #include <cstdio>
 #include <dccomms/CommsBridge.h>
-#include <dccomms/DataLinkFrame.h>
 #include <dccomms/Utils.h>
+#include <dccomms_packets/SimplePacket.h>
 #include <dccomms_utils/S100Stream.h>
 #include <iostream>
 
@@ -12,6 +12,7 @@
 
 using namespace std;
 using namespace dccomms;
+using namespace dccomms_packets;
 using namespace dccomms_utils;
 using namespace cpputils;
 
@@ -21,25 +22,31 @@ S100Stream *stream;
 
 int main(int argc, char **argv) {
   std::string modemPort;
-  uint32_t modemBitrate = 9600, portBaudrate = 9600;
+  uint32_t modemBitrate = 9600, txPacketSize = 20, rxPacketSize = 20, portBaudrate = 9600;
   std::string dccommsId;
   std::string logLevelStr, logFile;
   bool flush = false, syncLog = false, hwFlowControlEnabled = false;
-  Log->Info("S100 Bridge");
+  enum PktType { DLF = 0, SP = 1};
+  uint32_t txPktTypeInt = 1, rxPktTypeInt = 1;
+  Log->Info("s100bridge Bridge");
   try {
     cxxopts::Options options("dccomms_utils/s100_bridge",
                              " - command line options");
     options.add_options()
-        ("C,flow-control-enabled", "the flow control by hw is enabled in the S100 modem", cxxopts::value<bool>(hwFlowControlEnabled))
+        ("C,flow-control-enabled", "the flow control by hw is enabled in the modem", cxxopts::value<bool>(hwFlowControlEnabled))
         ("F,flush-log", "flush log", cxxopts::value<bool>(flush))
-        ("s,sync-log", "sync-log", cxxopts::value<bool>(syncLog))
-        ("f,log-file", "File to save the log", cxxopts::value<std::string>(logFile)->default_value("")->implicit_value("example2_log"))
+        ("s,sync-log", "sync-log (default: false -> async.)", cxxopts::value<bool>(syncLog))
+        ("f,log-file", "File to save the log", cxxopts::value<std::string>(logFile)->default_value("")->implicit_value("bridge_log"))
         ("p,modem-port", "Modem's serial port", cxxopts::value<std::string>(modemPort)->default_value("/dev/ttyUSB0"))
         ("b, baud-rate", "Serial port baudrate (default: 9600)", cxxopts::value<uint32_t>(portBaudrate))
         ("l,log-level", "log level: critical,debug,err,info,off,trace,warn", cxxopts::value<std::string>(logLevelStr)->default_value("info"))
         ("B, modem-bitrate", "maximum bitrate (used when hw flow control is disabled)", cxxopts::value<uint32_t>(modemBitrate))
         ("help", "Print help")
-        ("dccomms-id", "dccomms id for bridge", cxxopts::value<std::string>(dccommsId)->default_value("s100"));
+        ("dccomms-id", "dccomms id for bridge", cxxopts::value<std::string>(dccommsId)->default_value("s100"))
+        ("tx-packet-size", "transmitted SimplePacket size in bytes (=overhead+payload)", cxxopts::value<uint32_t>(txPacketSize))
+        ("rx-packet-size", "received SimplePacket size in bytes (=overhead+payload)", cxxopts::value<uint32_t>(rxPacketSize))
+        ("tx-packet-type", "0: DataLinkFrame, 1: SimplePacket (default).", cxxopts::value<uint32_t>(txPktTypeInt))
+        ("rx-packet-type", "0: DataLinkFrame, 1: SimplePacket (default).", cxxopts::value<uint32_t>(rxPktTypeInt));
 
     auto result = options.parse(argc, argv);
     if (result.count("help")) {
@@ -51,19 +58,56 @@ int main(int argc, char **argv) {
     std::cout << "error parsing options: " << e.what() << std::endl;
     exit(1);
   }
+  PktType txPktType = static_cast<PktType>(txPktTypeInt);
+  PktType rxPktType = static_cast<PktType>(rxPktTypeInt);
 
-  Log->Info("dccommsId: {} ; port: {} ; bit-rate: {} ; hw.FlowC: {}", dccommsId, modemPort,
-            modemBitrate, hwFlowControlEnabled);
+  PacketBuilderPtr rxpb, txpb;
+
+  switch(txPktType){
+    case DLF:{
+        auto checksumType = DataLinkFrame::fcsType::crc16;
+        txpb = CreateObject<DataLinkFramePacketBuilder>(checksumType);
+        break;
+    }
+    case SP:{
+        txpb = CreateObject<SimplePacketBuilder>(0, FCS::CRC16);
+        auto pkt = txpb->Create();
+        auto emptyPacketSize = pkt->GetPacketSize();
+        auto payloadSize = txPacketSize - emptyPacketSize;
+        txpb = CreateObject<SimplePacketBuilder>(payloadSize, FCS::CRC16);
+        break;
+    }
+    default:
+      std::cerr << "wrong tx packet type: "<< txPktType << std::endl;
+      return 1;
+  }
+  switch(rxPktType){
+    case DLF:{
+        auto checksumType = DataLinkFrame::fcsType::crc16;
+        rxpb = CreateObject<DataLinkFramePacketBuilder>(checksumType);
+        break;
+    }
+    case SP:{
+        rxpb = CreateObject<SimplePacketBuilder>(0, FCS::CRC16);
+        auto pk = rxpb->Create();
+        auto emptyPacketSize = pk->GetPacketSize();
+        auto payloadSize = rxPacketSize - emptyPacketSize;
+        rxpb = CreateObject<SimplePacketBuilder>(payloadSize, FCS::CRC16);
+        break;
+    }
+    default:
+      std::cerr << "wrong rx packet type: "<< rxPktType << std::endl;
+      return 1;
+  }
+  Log->Info("dccommsId: {} ; port: {} ; baudrate: {} ; hw.FlowC: {}", dccommsId, modemPort, portBaudrate,
+            hwFlowControlEnabled);
 
   LogLevel logLevel = cpplogging::GetLevelFromString(logLevelStr);
   auto baudrate = SerialPortStream::BaudRateFromUInt(portBaudrate);
   stream = new S100Stream(modemPort, baudrate, modemBitrate);
   stream->SetHwFlowControl(hwFlowControlEnabled);
 
-  PacketBuilderPtr pb = std::make_shared<DataLinkFramePacketBuilder>(
-      DataLinkFrame::fcsType::crc16);
-
-  bridge = new CommsBridge(stream, pb, pb, 0);
+  bridge = new CommsBridge(stream, txpb, rxpb, 0);
 
   bridge->SetLogLevel(logLevel);
   bridge->SetCommsDeviceId(dccommsId);
@@ -107,6 +151,7 @@ int main(int argc, char **argv) {
     Log->FlushLog();
     Utils::Sleep(2000);
     printf("Log messages flushed.\n");
+
     exit(0);
   });
 
--- src/main/dccomms_serialp_bridge/dccomms_serialp_bridge.cpp
+++ src/main/dccomms_serialp_bridge/dccomms_serialp_bridge.cpp
@@ -1,8 +1,8 @@
 #include <cpputils/SignalManager.h>
 #include <cstdio>
 #include <dccomms/CommsBridge.h>
-#include <dccomms/DataLinkFrame.h>
 #include <dccomms/Utils.h>
+#include <dccomms_packets/SimplePacket.h>
 #include <dccomms/SerialPortStream.h>
 #include <iostream>
 
@@ -12,6 +12,7 @@
 
 using namespace std;
 using namespace dccomms;
+using namespace dccomms_packets;
 using namespace cpputils;
 
 LoggerPtr Log = cpplogging::CreateLogger("SerialPortBridgeMain");
@@ -20,10 +21,12 @@ SerialPortStream *stream;
 
 int main(int argc, char **argv) {
   std::string modemPort;
+  uint32_t txPacketSize = 20, rxPacketSize = 20, portBaudrate = 9600;
   std::string dccommsId;
   std::string logLevelStr, logFile;
-  uint32_t portBaudrate = 9600;
-  bool flush = false, asyncLog = true, hwFlowControlEnabled = false;
+  bool flush = false, syncLog = false, hwFlowControlEnabled = false;
+  enum PktType { DLF = 0, SP = 1};
+  uint32_t txPktTypeInt = 1, rxPktTypeInt = 1;
   Log->Info("SerialPort Bridge");
   try {
     cxxopts::Options options("dccomms_utils/serialp_bridge",
@@ -31,13 +34,17 @@ int main(int argc, char **argv) {
     options.add_options()
         ("C,flow-control-enabled", "the flow control by hw is enabled in the modem", cxxopts::value<bool>(hwFlowControlEnabled))
         ("F,flush-log", "flush log", cxxopts::value<bool>(flush))
-        ("a,async-log", "async-log", cxxopts::value<bool>(asyncLog))
-        ("f,log-file", "File to save the log", cxxopts::value<std::string>(logFile)->default_value("")->implicit_value("example2_log"))
+        ("s,sync-log", "sync-log (default: false -> async.)", cxxopts::value<bool>(syncLog))
+        ("f,log-file", "File to save the log", cxxopts::value<std::string>(logFile)->default_value("")->implicit_value("bridge_log"))
         ("p,modem-port", "Modem's serial port", cxxopts::value<std::string>(modemPort)->default_value("/dev/ttyUSB0"))
         ("b, baud-rate", "Serial port baudrate (default: 9600)", cxxopts::value<uint32_t>(portBaudrate))
         ("l,log-level", "log level: critical,debug,err,info,off,trace,warn", cxxopts::value<std::string>(logLevelStr)->default_value("info"))
         ("help", "Print help")
-        ("dccomms-id", "dccomms id for bridge", cxxopts::value<std::string>(dccommsId)->default_value("s100"));
+        ("dccomms-id", "dccomms id for bridge", cxxopts::value<std::string>(dccommsId)->default_value("s100"))
+        ("tx-packet-size", "transmitted SimplePacket size in bytes (=overhead+payload)", cxxopts::value<uint32_t>(txPacketSize))
+        ("rx-packet-size", "received SimplePacket size in bytes (=overhead+payload)", cxxopts::value<uint32_t>(rxPacketSize))
+        ("tx-packet-type", "0: DataLinkFrame, 1: SimplePacket (default).", cxxopts::value<uint32_t>(txPktTypeInt))
+        ("rx-packet-type", "0: DataLinkFrame, 1: SimplePacket (default).", cxxopts::value<uint32_t>(rxPktTypeInt));
 
     auto result = options.parse(argc, argv);
     if (result.count("help")) {
@@ -49,18 +56,55 @@ int main(int argc, char **argv) {
     std::cout << "error parsing options: " << e.what() << std::endl;
     exit(1);
   }
+  PktType txPktType = static_cast<PktType>(txPktTypeInt);
+  PktType rxPktType = static_cast<PktType>(rxPktTypeInt);
 
-  Log->Info("dccommsId: {} ; port: {} ; bit-rate: {} ; hw.FlowC: {}", dccommsId, modemPort,
+  PacketBuilderPtr rxpb, txpb;
+
+  switch(txPktType){
+    case DLF:{
+        auto checksumType = DataLinkFrame::fcsType::crc16;
+        txpb = CreateObject<DataLinkFramePacketBuilder>(checksumType);
+        break;
+    }
+    case SP:{
+        txpb = CreateObject<SimplePacketBuilder>(0, FCS::CRC16);
+        auto pkt = txpb->Create();
+        auto emptyPacketSize = pkt->GetPacketSize();
+        auto payloadSize = txPacketSize - emptyPacketSize;
+        txpb = CreateObject<SimplePacketBuilder>(payloadSize, FCS::CRC16);
+        break;
+    }
+    default:
+      std::cerr << "wrong tx packet type: "<< txPktType << std::endl;
+      return 1;
+  }
+  switch(rxPktType){
+    case DLF:{
+        auto checksumType = DataLinkFrame::fcsType::crc16;
+        rxpb = CreateObject<DataLinkFramePacketBuilder>(checksumType);
+        break;
+    }
+    case SP:{
+        rxpb = CreateObject<SimplePacketBuilder>(0, FCS::CRC16);
+        auto pk = rxpb->Create();
+        auto emptyPacketSize = pk->GetPacketSize();
+        auto payloadSize = rxPacketSize - emptyPacketSize;
+        rxpb = CreateObject<SimplePacketBuilder>(payloadSize, FCS::CRC16);
+        break;
+    }
+    default:
+      std::cerr << "wrong rx packet type: "<< rxPktType << std::endl;
+      return 1;
+  }
+  Log->Info("dccommsId: {} ; port: {} ; baudrate: {} ; hw.FlowC: {}", dccommsId, modemPort, portBaudrate,
             hwFlowControlEnabled);
 
   LogLevel logLevel = cpplogging::GetLevelFromString(logLevelStr);
   stream = new SerialPortStream(modemPort, portBaudrate);
   stream->SetHwFlowControl(hwFlowControlEnabled);
 
-  PacketBuilderPtr pb = std::make_shared<DataLinkFramePacketBuilder>(
-      DataLinkFrame::fcsType::crc16);
-
-  bridge = new CommsBridge(stream, pb, pb, 0);
+  bridge = new CommsBridge(stream, txpb, rxpb, 0);
 
   bridge->SetLogLevel(logLevel);
   bridge->SetCommsDeviceId(dccommsId);
@@ -82,7 +126,7 @@ int main(int argc, char **argv) {
     Log->FlushLogOn(info);
     Log->Info("Flush log on info");
   }
-  if (asyncLog) {
+  if (!syncLog) {
     Log->SetAsyncMode();
     Log->Info("Async. log");
   }
@@ -104,6 +148,7 @@ int main(int argc, char **argv) {
     Log->FlushLog();
     Utils::Sleep(2000);
     printf("Log messages flushed.\n");
+
     exit(0);
   });
 
