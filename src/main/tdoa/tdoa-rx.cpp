2a1b0a71c8bb9d9461f1179cfa6cf857d98026cc
dcentelles <centelld@uji.es>
2019-11-20 13:00:23 +0100

    update tdoa for radiomodem

--- src/main/tdoa/tdoa-rx.cpp
+++ src/main/tdoa/tdoa-rx.cpp
@@ -3,7 +3,7 @@
 #include <cpputils/Timer.h>
 #include <cstdio>
 #include <cstring>
-#include <dccomms/Arduino.h>
+#include <dccomms/SerialPortStream.h>
 #include <dccomms/DataLinkFrame.h>
 
 #include <cstdio>
@@ -14,172 +14,143 @@ using namespace std;
 using namespace dccomms;
 using namespace cpputils;
 
-LoggerPtr Log = cpplogging::CreateLogger("tdoa-rx");
-
 int main(int argc, char **argv) {
-  std::string ac_modemPort, rf_modemPort;
-  uint32_t ac_portBaudrate = 9600, rf_portBaudrate = 9600;
-  std::string dccommsId;
-  std::string logLevelStr, logFile;
-  bool flush = false, syncLog = false, hwFlowControlEnabled = false;
-  Log->Info("TDoA-RX");
-  try {
-    cxxopts::Options options("dccomms_utils/s100_bridge",
-                             " - command line options");
-    options.add_options()("C,flow-control-enabled",
-                          "the flow control by hw is enabled in the modem",
-                          cxxopts::value<bool>(hwFlowControlEnabled))(
-        "F,flush-log", "flush log", cxxopts::value<bool>(flush))(
-        "s,sync-log", "sync-log (default: false -> async.)",
-        cxxopts::value<bool>(syncLog))(
-        "f,log-file", "File to save the log",
-        cxxopts::value<std::string>(logFile)->default_value("")->implicit_value(
-            "bridge_log"))("ac-modem-port", "AC Modem's serial port",
-                           cxxopts::value<std::string>(ac_modemPort)
-                               ->default_value("/dev/ttyUSB0"))(
-        "rf-modem-port", "RF Modem's serial port",
-        cxxopts::value<std::string>(rf_modemPort)
-            ->default_value("/dev/ttyUSB1"))(
-        "ac-baud-rate", "AC Serial port baudrate (default: 9600)",
-        cxxopts::value<uint32_t>(ac_portBaudrate))(
-        "rf-baud-rate", "RF Serial port baudrate (default: 9600)",
-        cxxopts::value<uint32_t>(rf_portBaudrate))(
-        "l,log-level", "log level: critical,debug,err,info,off,trace,warn",
-        cxxopts::value<std::string>(logLevelStr)->default_value("info"))(
-        "help", "Print help");
+    LoggerPtr Log = cpplogging::CreateLogger("TDoA-rx");
+    LoggerPtr CsvLog = cpplogging::CreateLogger("CSV");
+    CsvLog->SetAsyncMode();
+    std::string ac_modemPort, rf_modemPort;
+    uint32_t ac_portBaudrate = 9600, rf_portBaudrate = 115200;
+    std::string dccommsId;
+    std::string logLevelStr, logFile;
+    uint32_t samples = 10;
+    std::string csvfile = "data.csv";
+    bool flush = false, syncLog = false, hwFlowControlEnabled = false;
+    Log->Info("TDoA-rx");
+    try {
+        cxxopts::Options options("dccomms_utils/tdoa-rx",
+                                 " - command line options");
+        options.add_options()
+            ("C,flow-control-enabled", "the flow control by hw is enabled in the modem", cxxopts::value<bool>(hwFlowControlEnabled))
+            ("F,flush-log", "flush log", cxxopts::value<bool>(flush))
+            ("s,sync-log", "sync-log (default: false -> async.)", cxxopts::value<bool>(syncLog))
+            ("f,log-file", "File to save the log",cxxopts::value<std::string>(logFile)->default_value("")->implicit_value("bridge_log"))
+            ("csv", "sample files", cxxopts::value<std::string>(csvfile))
+            ("ac-modem-port", "AC Modem's serial port",cxxopts::value<std::string>(ac_modemPort)->default_value("/dev/ttyUSB0"))
+            ("ac-baud-rate", "AC Serial port baudrate (default: 9600)",cxxopts::value<uint32_t>(ac_portBaudrate))
+            ("rf-modem-port", "RF Modem's serial port",cxxopts::value<std::string>(rf_modemPort)->default_value("/dev/ttyACM0"))
+            ("rf-baud-rate", "RF Serial port baudrate (default: 115200)",cxxopts::value<uint32_t>(rf_portBaudrate))
+            ("samples", "default 10", cxxopts::value<uint32_t>(samples))
+            ("l,log-level", "log level: critical,debug,err,info,off,trace,warn",cxxopts::value<std::string>(logLevelStr)->default_value("info"))
+            ("help", "Print help");
 
-    auto result = options.parse(argc, argv);
-    if (result.count("help")) {
-      std::cout << options.help({""}) << std::endl;
-      exit(0);
+        auto result = options.parse(argc, argv);
+        if (result.count("help")) {
+            std::cout << options.help({""}) << std::endl;
+            exit(0);
+        }
+
+    } catch (const cxxopts::OptionException &e) {
+        std::cout << "error parsing options: " << e.what() << std::endl;
+        exit(1);
     }
 
-  } catch (const cxxopts::OptionException &e) {
-    std::cout << "error parsing options: " << e.what() << std::endl;
-    exit(1);
-  }
+    Log->Info("AC port: {} ; AC baudrate: {} ; RF port: {} ; RF baudrate: {}", ac_modemPort,
+              ac_portBaudrate, rf_modemPort, rf_portBaudrate);
 
-  Log->Info("AC: port: {} ; baudrate: {} ; hw.FlowC: {}", ac_modemPort,
-            ac_portBaudrate, hwFlowControlEnabled);
-  Log->Info("RF: port: {} ; baudrate: {} ; hw.FlowC: {}", rf_modemPort,
-            rf_portBaudrate, hwFlowControlEnabled);
+    auto ac_baudrate = SerialPortStream::BaudRateFromUInt(ac_portBaudrate);
+    auto rf_baudrate = SerialPortStream::BaudRateFromUInt(rf_portBaudrate);
 
-  LogLevel logLevel = cpplogging::GetLevelFromString(logLevelStr);
-  auto ac_baudrate = SerialPortStream::BaudRateFromUInt(ac_portBaudrate);
-  auto rf_baudrate = SerialPortStream::BaudRateFromUInt(rf_portBaudrate);
-
-  auto logFormatter = std::make_shared<spdlog::pattern_formatter>("%T.%F %v");
-  Log->SetLogFormatter(logFormatter);
-
-  if (logFile != "") {
-    Log->LogToFile(logFile);
-  }
-  if (flush) {
-    Log->FlushLogOn(info);
-    Log->Info("Flush log on info");
-  }
-  if (!syncLog) {
-    Log->SetAsyncMode();
-    Log->Info("Async. log");
-  }
-Log->LogToConsole(true);
-auto ac_stream =
-    CreateObject<SerialPortStream>(ac_modemPort.c_str(), ac_baudrate);
-
-auto rf_stream =
-    CreateObject<SerialPortStream>(rf_modemPort.c_str(), rf_baudrate);
-
-//  Arduino arduino = Arduino::FindArduino(Arduino::BAUD_115200,
-//                                        "Hello, are you TX?\n",
-//                                        "Yes, I'm TX");
-
-//  auto rf_stream = cpputils::Ptr<Arduino>(&arduino);
-
-//ac_stream->Open();
-rf_stream->Open();
-//  if(!arduino.IsOpen())
-//  {
-//      Log->Error("arduino not found");
-//  }
-  auto checksumType = DataLinkFrame::fcsType::crc16;
-  auto pb = CreateObject<DataLinkFramePacketBuilder>(checksumType);
-  auto pkt = pb->Create();
-
-  int cont = 0;
-  char message[100];
-  int br = 5;
-  message[1] = 0;
-  ac_stream->FlushInput();
-  rf_stream->FlushInput();
-  char ac_pre[50] = "#B0021";
-  size_t ac_pre_len = std::strlen(ac_pre);
-  uint8_t rf_pre[50] = {'1', 0xd, 0xa};
-  size_t rf_pre_len = 3;
-  cpputils::Timer td;
-  uint elapsed;
-  std::thread main([&]() {
-    while (1) {
-      rf_stream->FlushIO();
-      //rf_stream->Read(message, rf_pre_len);
-      rf_stream >> pkt;
-      //ac_stream->FlushIO();
-
-      td.Reset();
-      //      message[rf_pre_len] = 0;
-      //      Log->Info("RF: {} -- {}", message,
-      //                ac_stream->Available());
-      message[1] = 0;
-      if(pkt->PacketIsOk())
-      {
-          Log->Info("RF RX");
-          //      ac_stream->WaitFor((const uint8_t *)ac_pre, ac_pre_len);
-          //      int res = ac_stream->Read(message, 3);
-//          message[1] = 0;
-//          elapsed = td.Elapsed();
-//          Log->Info("message: {} ; TDoA: {}", message, elapsed);
-
-          //      ac_stream->WaitFor((const uint8_t *)ac_pre, ac_pre_len);
-          //      int res = ac_stream->Read(message, 3);
-          //      message[1] = 0;
-          //      elapsed = td.Elapsed();
-          //      Log->Info("message: {} ; TDoA: {}", message, elapsed);
-      }else
-      {
-          Log->Warn("RF ERR");
-
-      }
+    if (logFile != "") {
+        Log->LogToFile(logFile);
     }
-  });
-
-  std::thread aux([&]() {
-    while (1) {
-      //      Log->Info("Cont: {}", cont);
-      std::this_thread::sleep_for(std::chrono::seconds(8));
+    if (flush) {
+        Log->FlushLogOn(info);
+        Log->Info("Flush log on info");
     }
-  });
+    if (!syncLog) {
+        Log->SetAsyncMode();
+        Log->Info("Async. log");
+    }
 
-  SignalManager::SetLastCallback(SIGINT, [&](int sig) {
-    printf("Received %d signal\nClosing device socket...\n", sig);
-    printf("Device closed.\n");
-    fflush(stdout);
-    Log->FlushLog();
-    std::this_thread::sleep_for(std::chrono::seconds(2));
-    printf("Log messages flushed.\n");
+    auto ac0_stream = CreateObject<SerialPortStream>(ac_modemPort, ac_baudrate);
+    auto rf_stream = CreateObject<SerialPortStream>(rf_modemPort, rf_baudrate);
 
-    exit(0);
-  });
-  main.join();
-  aux.join();
+    ac0_stream->Open();
+    rf_stream->Open();
+    CsvLog->LogToFile(csvfile);
+    CsvLog->LogToConsole(true);
+    Log->SetLogFormatter(std::make_shared<spdlog::pattern_formatter>("%T.%F %v"));
+    Log->LogToConsole(true);
+    CsvLog->SetLogFormatter(std::make_shared<spdlog::pattern_formatter>("%T.%F %v"));
+
+    auto checksumType = DataLinkFrame::fcsType::crc16;
+    auto pb = CreateObject<DataLinkFramePacketBuilder>(checksumType);
+    auto pkt = pb->Create();
+    uint8_t * rf_count_hl = pkt->GetPayloadBuffer();
+    uint8_t * rf_count_h = rf_count_hl;
+    uint8_t * rf_count_l = rf_count_hl+1;
+
+    ac0_stream->FlushInput();
+    char ac_pre[50] = "#B0012";
+    size_t ac_pre_len = std::strlen(ac_pre);
+    cpputils::TimerNanos td;
+    uint64_t elapsed;
+    uint32_t cursample = 0;
+    uint16_t count = 0;
+    uint16_t rx_count = 0xffff, rf_rx_count = 0xffff;
+    int first_its = 2;
+    uint8_t count_hl[4];
+    uint8_t * count_h = count_hl;
+    uint8_t * count_l = count_hl+1;
+    std::thread main([&]() {
+        while (1) {
+            ac0_stream->FlushIO();
+            rf_stream->FlushIO();
+            rf_stream >> pkt;
+            if(pkt->PacketIsOk())
+            {
+                td.Reset();
+                rf_rx_count = ((uint16_t)*rf_count_h << 8) | *rf_count_l;
+
+                CsvLog->Info("RF {}", rf_rx_count);
+
+                ac0_stream->WaitFor((const uint8_t *)ac_pre, ac_pre_len);
+                int res = ac0_stream->Read(&count_hl, sizeof(count) + 2);
+                rx_count = ((uint16_t)*count_h << 8) | *count_l;
+                if(rf_rx_count == rx_count)
+                {
+                    elapsed = td.Elapsed();
+                    if (count >= first_its) {
+                        cursample += 1;
+                        CsvLog->Info("AC {} {} {:.6f}", rx_count, elapsed, elapsed/1e6);
+                        if (cursample == samples) {
+                            Log->Info("END");
+                            break;
+                        }
+                    } else {
+                        Log->Info("SYNC: {}", rx_count);
+                    }
+                    count++;
+                }
+                else{
+                    Log->Warn("Counter mismatch {} {}", rf_rx_count, rx_count);
+                }
+            }
+            else{
+                Log->Warn("Packet error");
+            }
+        }
+    });
+
+    SignalManager::SetLastCallback(SIGINT, [&](int sig) {
+        printf("Received %d signal\nClosing device socket...\n", sig);
+        printf("Device closed.\n");
+        fflush(stdout);
+        Log->FlushLog();
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        printf("Log messages flushed.\n");
+
+        exit(0);
+    });
+    main.join();
 }
-
-// rf_stream->Read(message, rf_pre_len);
-// ac_stream->FlushIO();
-// td.Reset();
-// message[rf_pre_len] = 0;
-// Log->Info("RF: {} -- {}", message,
-//          ac_stream->Available());
-////ac_stream->WaitFor((const uint8_t *)ac_pre, ac_pre_len);
-// int res = ac_stream->Read(message, ac_pre_len + 3);
-// message[ac_pre_len + 1] = 0;
-// elapsed = td.Elapsed();
-// Log->Info("message: {} ; TDoA: {}", message, elapsed);
--- src/main/tdoa/tdoa-tx.cpp
+++ src/main/tdoa/tdoa-tx.cpp
@@ -2,7 +2,8 @@
 #include <cpputils/SignalManager.h>
 #include <cpputils/Timer.h>
 #include <cstdio>
-#include <dccomms/Arduino.h>
+#include <cstring>
+#include <dccomms/SerialPortStream.h>
 #include <dccomms/DataLinkFrame.h>
 
 #include <cstdio>
@@ -13,130 +14,130 @@ using namespace std;
 using namespace dccomms;
 using namespace cpputils;
 
-LoggerPtr Log = cpplogging::CreateLogger("tdoa-tx");
-
 int main(int argc, char **argv) {
-  std::string ac_modemPort, rf_modemPort;
-  uint32_t ac_portBaudrate = 9600, rf_portBaudrate = 9600;
-  std::string dccommsId;
-  std::string logLevelStr, logFile;
-  bool flush = false, syncLog = false, hwFlowControlEnabled = false;
-  Log->Info("TDoA-TX");
-  try {
-    cxxopts::Options options("dccomms_utils/s100_bridge",
-                             " - command line options");
-    options.add_options()("C,flow-control-enabled",
-                          "the flow control by hw is enabled in the modem",
-                          cxxopts::value<bool>(hwFlowControlEnabled))(
-        "F,flush-log", "flush log", cxxopts::value<bool>(flush))(
-        "s,sync-log", "sync-log (default: false -> async.)",
-        cxxopts::value<bool>(syncLog))(
-        "f,log-file", "File to save the log",
-        cxxopts::value<std::string>(logFile)->default_value("")->implicit_value(
-            "bridge_log"))("ac-modem-port", "AC Modem's serial port",
-                           cxxopts::value<std::string>(ac_modemPort)
-                               ->default_value("/dev/ttyUSB0"))(
-        "rf-modem-port", "RF Modem's serial port",
-        cxxopts::value<std::string>(rf_modemPort)
-            ->default_value("/dev/ttyUSB1"))(
-        "ac-baud-rate", "AC Serial port baudrate (default: 9600)",
-        cxxopts::value<uint32_t>(ac_portBaudrate))(
-        "rf-baud-rate", "RF Serial port baudrate (default: 9600)",
-        cxxopts::value<uint32_t>(rf_portBaudrate))(
-        "l,log-level", "log level: critical,debug,err,info,off,trace,warn",
-        cxxopts::value<std::string>(logLevelStr)->default_value("info"))(
-        "help", "Print help");
+    LoggerPtr Log = cpplogging::CreateLogger("TDoA-tx");
+    LoggerPtr CsvLog = cpplogging::CreateLogger("CSV");
+    CsvLog->SetAsyncMode();
+    std::string ac_modemPort, rf_modemPort;
+    uint32_t ac_portBaudrate = 9600, rf_portBaudrate = 115200;
+    std::string dccommsId;
+    std::string logLevelStr, logFile;
+    uint32_t samples = 10;
+    std::string csvfile = "data.csv";
+    bool flush = false, syncLog = false, hwFlowControlEnabled = false;
+    Log->Info("TDoA-tx");
+    try {
+        cxxopts::Options options("dccomms_utils/tdoa-tx",
+                                 " - command line options");
+        options.add_options()
+            ("C,flow-control-enabled", "the flow control by hw is enabled in the modem", cxxopts::value<bool>(hwFlowControlEnabled))
+            ("F,flush-log", "flush log", cxxopts::value<bool>(flush))
+            ("s,sync-log", "sync-log (default: false -> async.)", cxxopts::value<bool>(syncLog))
+            ("f,log-file", "File to save the log",cxxopts::value<std::string>(logFile)->default_value("")->implicit_value("bridge_log"))
+            ("csv", "sample files", cxxopts::value<std::string>(csvfile))
+            ("ac-modem-port", "AC Modem's serial port",cxxopts::value<std::string>(ac_modemPort)->default_value("/dev/ttyUSB0"))
+            ("ac-baud-rate", "AC Serial port baudrate (default: 9600)",cxxopts::value<uint32_t>(ac_portBaudrate))
+            ("rf-modem-port", "RF Modem's serial port",cxxopts::value<std::string>(rf_modemPort)->default_value("/dev/ttyACM0"))
+            ("rf-baud-rate", "RF Serial port baudrate (default: 115200)",cxxopts::value<uint32_t>(rf_portBaudrate))
+            ("samples", "default 10", cxxopts::value<uint32_t>(samples))
+            ("l,log-level", "log level: critical,debug,err,info,off,trace,warn",cxxopts::value<std::string>(logLevelStr)->default_value("info"))
+            ("help", "Print help");
 
-    auto result = options.parse(argc, argv);
-    if (result.count("help")) {
-      std::cout << options.help({""}) << std::endl;
-      exit(0);
+        auto result = options.parse(argc, argv);
+        if (result.count("help")) {
+            std::cout << options.help({""}) << std::endl;
+            exit(0);
+        }
+
+    } catch (const cxxopts::OptionException &e) {
+        std::cout << "error parsing options: " << e.what() << std::endl;
+        exit(1);
     }
 
-  } catch (const cxxopts::OptionException &e) {
-    std::cout << "error parsing options: " << e.what() << std::endl;
-    exit(1);
-  }
+    Log->Info("AC port: {} ; AC baudrate: {} ; RF port: {} ; RF baudrate: {}", ac_modemPort,
+              ac_portBaudrate, rf_modemPort, rf_portBaudrate);
 
-  Log->Info("AC: port: {} ; baudrate: {} ; hw.FlowC: {}", ac_modemPort,
-            ac_portBaudrate, hwFlowControlEnabled);
-  Log->Info("RF: port: {} ; baudrate: {} ; hw.FlowC: {}", rf_modemPort,
-            rf_portBaudrate, hwFlowControlEnabled);
+    auto ac_baudrate = SerialPortStream::BaudRateFromUInt(ac_portBaudrate);
+    auto rf_baudrate = SerialPortStream::BaudRateFromUInt(rf_portBaudrate);
 
-  LogLevel logLevel = cpplogging::GetLevelFromString(logLevelStr);
-  auto ac_baudrate = SerialPortStream::BaudRateFromUInt(ac_portBaudrate);
-  auto rf_baudrate = SerialPortStream::BaudRateFromUInt(rf_portBaudrate);
+    if (logFile != "") {
+        Log->LogToFile(logFile);
+    }
+    if (flush) {
+        Log->FlushLogOn(info);
+        Log->Info("Flush log on info");
+    }
+    if (!syncLog) {
+        Log->SetAsyncMode();
+        Log->Info("Async. log");
+    }
 
-  auto logFormatter = std::make_shared<spdlog::pattern_formatter>("%T.%F %v");
-  Log->SetLogFormatter(logFormatter);
+    auto ac0_stream = CreateObject<SerialPortStream>(ac_modemPort, ac_baudrate);
+    auto rf_stream = CreateObject<SerialPortStream>(rf_modemPort, rf_baudrate);
 
-  if (logFile != "") {
-    Log->LogToFile(logFile);
-  }
-  if (flush) {
-    Log->FlushLogOn(info);
-    Log->Info("Flush log on info");
-  }
-  if (!syncLog) {
-    Log->SetAsyncMode();
-    Log->Info("Async. log");
-  }
+    ac0_stream->Open();
+    rf_stream->Open();
+    CsvLog->LogToFile(csvfile);
+    CsvLog->LogToConsole(true);
+    Log->SetLogFormatter(std::make_shared<spdlog::pattern_formatter>("%T.%F %v"));
+    Log->LogToConsole(true);
+    CsvLog->SetLogFormatter(std::make_shared<spdlog::pattern_formatter>("%T.%F %v"));
 
-  auto ac_stream =
-      CreateObject<SerialPortStream>(ac_modemPort.c_str(), ac_baudrate);
+    auto checksumType = DataLinkFrame::fcsType::crc16;
+    auto pb = CreateObject<DataLinkFramePacketBuilder>(checksumType);
+    auto pkt = pb->Create();
+    uint8_t * rf_count_hl = pkt->GetPayloadBuffer();
+    uint8_t * rf_count_h = rf_count_hl;
+    uint8_t * rf_count_l = rf_count_hl+1;
 
-  auto rf_stream =
-      CreateObject<SerialPortStream>(rf_modemPort.c_str(), rf_baudrate);
+    uint8_t count_hl[2];
+    uint8_t * count_h = count_hl;
+    uint8_t * count_l = count_hl+1;
 
-//  Arduino arduino = Arduino::FindArduino(Arduino::BAUD_115200,
-//                                        "Hello, are you TX?\n",
-//                                        "Yes, I'm TX");
+    char message[100];
+    message[1] = 0;
+    ac0_stream->FlushInput();
+    cpputils::TimerNanos td;
+    uint16_t count = 0;
+    int first_its = 1;
+    uint16_t max = samples + first_its;
+    std::thread main([&]() {
+        while (count <= max) {
+            ac0_stream->FlushIO();
+            rf_stream->FlushIO();
 
-//  auto rf_stream = cpputils::Ptr<Arduino>(&arduino);
+            *rf_count_h = count >> 8;
+            *rf_count_l = count & 0x00ff;
 
-  //ac_stream->Open();
-  rf_stream->Open();
-//  if(!arduino.IsOpen())
-//  {
-//      Log->Error("arduino not found");
-//  }
+            //SEND RF MESSAGE
+            pkt->PayloadUpdated(2);
+            rf_stream << pkt;
 
-  Log->LogToConsole(true);
+            //SEND ACOUSTIC MESSAGE
+            ac0_stream << "$B2";
+            *count_h = count >> 8;
+            *count_l = count & 0x00ff;
+            ac0_stream->Write(&count_hl, 2);
+            if(first_its < count)
+                CsvLog->Info("{}", count);
+            else
+                Log->Info("SYNC: {}", count);
+            count++;
 
-  auto checksumType = DataLinkFrame::fcsType::crc16;
-  auto pb = CreateObject<DataLinkFramePacketBuilder>(checksumType);
-  auto pkt = pb->Create();
-  std::string strmsg = "Hello World";
-  pkt->SetPayload((uint8_t*)strmsg.c_str(), strmsg.size());
-  pkt->PayloadUpdated(strmsg.size());
+            std::this_thread::sleep_for(std::chrono::milliseconds(1500));
+        }
+        Log->Info("END");
+    });
 
-  int cont = 0;
-  uint8_t rf_pre[50] = {'1', 0xd, 0xa};
-  char msg;
-  cpputils::Timer td;
-  uint elapsed;
-  while (1) {
-//    msg = 'a' + cont;
-//    td.Reset();
-//    //ac_stream << "$B1" << msg;
-//    rf_pre[0] = msg;
-    //rf_stream->Write(rf_pre, 3);
-Log->Info("TX {}", pkt->GetPacketSize());
-    rf_stream << pkt;
-//    elapsed = td.Elapsed();
-//    Log->Info("T2: {}",  msg);
-//    cont++;
-    std::this_thread::sleep_for(std::chrono::seconds(6));
-  }
+    SignalManager::SetLastCallback(SIGINT, [&](int sig) {
+        printf("Received %d signal\nClosing device socket...\n", sig);
+        printf("Device closed.\n");
+        fflush(stdout);
+        Log->FlushLog();
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        printf("Log messages flushed.\n");
 
-  SignalManager::SetLastCallback(SIGINT, [&](int sig) {
-    printf("Received %d signal\nClosing device socket...\n", sig);
-    printf("Device closed.\n");
-    fflush(stdout);
-    Log->FlushLog();
-    std::this_thread::sleep_for(std::chrono::seconds(2));
-    printf("Log messages flushed.\n");
-
-    exit(0);
-  });
+        exit(0);
+    });
+    main.join();
 }
